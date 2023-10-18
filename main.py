fc23216f4d81ae3ca014a779ed4915b448dbaebf
saslol10 <krhamidullin@mail.ru>
2023-10-18 22:22:09 +0400

    1

--- main.py
+++ main.py
@@ -19,15 +19,15 @@ a3 = (t3 * (t2 ** 2) - t2 * (t3 ** 2) + 2 * t1 * (t3 ** 2) + 3 * t2 * (t1 ** 2) 
 p = ((3 * a3 * a1) - (a2 ** 2)) / (3 * (a3 ** 2))
 q = ((2 * (a2 ** 3)) - 9 * a3 * a2 * a1 + 27 * (a3 ** 2) * (-4)) / (27 * (a3 ** 3))
 
-Q = ((p / 3) ** 3) + ((q / 2) ** 2)
+D = ((p / 3) ** 3) + ((q / 2) ** 2)
 
 flag = 0
 
-if Q < 0:
+if D < 0:
     if q < 0:
-        phi = math.atan(math.sqrt(-Q) / (q / 2))
+        phi = math.atan(math.sqrt(-D) / (q / 2))
     elif q > 0:
-        phi = math.atan(math.sqrt(-Q) / (q / 2)) + math.pi
+        phi = math.atan(math.sqrt(-D) / (q / 2)) + math.pi
     elif q == 0:
         phi = math.pi / 2
     y1 = 2 * math.sqrt(-(p / 3)) * math.cos(phi / 3)
@@ -43,12 +43,12 @@ if Q < 0:
 #    plt.plot([t0, t1, t2, t3, max(t3, x1, x2, x3)], [0, 1, 2, 3, 4])
 #    plt.show()
 
-if Q > 0:
-    y1 = (((-(q/2)) + math.sqrt(Q)) ** (1/3)) + (((-(q/2)) - math.sqrt(Q)) ** (1/3))
+if D > 0:
+    y1 = (((-(q/2)) + math.sqrt(D)) ** (1 / 3)) + (((-(q / 2)) - math.sqrt(D)) ** (1 / 3))
 
-    y2 = (-1/2) * ((((-(q/2)) + math.sqrt(Q)) ** (1/3)) + (((-(q/2)) - math.sqrt(Q)) ** (1/3))) + complex(0, math.sqrt(3)/2) * ((((-(q/2)) + math.sqrt(Q)) ** (1/3)) + (((-(q/2)) - math.sqrt(Q)) ** (1/3)))
+    y2 = (-1/2) * ((((-(q/2)) + math.sqrt(D)) ** (1 / 3)) + (((-(q / 2)) - math.sqrt(D)) ** (1 / 3))) + complex(0, math.sqrt(3) / 2) * ((((-(q / 2)) + math.sqrt(D)) ** (1 / 3)) + (((-(q / 2)) - math.sqrt(D)) ** (1 / 3)))
 
-    y3 = (-1/2) * ((((-(q/2)) + math.sqrt(Q)) ** (1/3)) + (((-(q/2)) - math.sqrt(Q)) ** (1/3))) - complex(0, math.sqrt(3)/2) * ((((-(q/2)) + math.sqrt(Q)) ** (1/3)) + (((-(q/2)) - math.sqrt(Q)) ** (1/3)))
+    y3 = (-1/2) * ((((-(q/2)) + math.sqrt(D)) ** (1 / 3)) + (((-(q / 2)) - math.sqrt(D)) ** (1 / 3))) - complex(0, math.sqrt(3) / 2) * ((((-(q / 2)) + math.sqrt(D)) ** (1 / 3)) + (((-(q / 2)) - math.sqrt(D)) ** (1 / 3)))
 
     x1 = y1 - a2 / (3 * a3)
     x2 = y2 - a2 / (3 * a3)
@@ -56,7 +56,7 @@ if Q > 0:
     print("Q > 0")
     print(x1, x2, x3)
     flag = 2
-if Q == 0:
+if D == 0:
     if q == 0 and p == 0:
         y1 = 0
         x1 = - a2 / (3 * a3)
